64171ef304952852433fbbc206e882c3e44d9031
nickmagruder <nick.magruder@outlook.com>
2021-07-19 23:26:45 -0700

    trees

---
+++ trees/worksheet1.js
@@ -0,0 +1,133 @@
+/* 
+
+Big O of BST
+Must be sorted
+
+Best and average cases:
+
+Insertion - O(log n)
+Searching - O(log n)
+ 
+(But not guaranteed)
+
+Worst case? Some configurations are slow, worst case, a "single-line" BST that is effectively a linked-list
+
+
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+//////////////////////////   INSERT   ////////////////////////// 
+
+  insert(value) {
+
+    let newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    while (true) {
+
+      if (value === current.value) return undefined;
+
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+
+        current = current.left;
+
+      } else {
+
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+
+        current = current.right;
+
+      }
+    }
+  }
+
+//////////////////////////   FIND  ////////////////////////// 
+
+  find(value) {
+
+    if (this.root === null) return false;
+
+    let current = this.root,
+      found = false;
+
+    while (current && !found) {      // while "there is" a current, and "found" is false
+      if (value < current.value) {
+        current = current.left;
+
+      } else if (value > current.value) {
+        current = current.right;
+
+      } else {
+        found = true;
+      }
+    }
+
+    if (!found) return undefined;
+    return current;
+  }
+
+  //////////////////////////   CONTAINS?  ////////////////////////// 
+
+  contains(value) {
+
+    if (this.root === null) return false;
+
+    let current = this.root,
+      found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+
+      } else if (value > current.value) {
+        current = current.right;
+
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+
+
+
+let tree = new BinarySearchTree();
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
+
+
+
+
