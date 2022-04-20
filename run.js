724b81c577722f6683b1731c3a3aab0175fbdea2
nickmagruder <nick.magruder@outlook.com>
2022-04-19 22:46:38 -0700

    arrays

--- run.js
+++ run.js
@@ -1,54 +1,29 @@
-'use strict';
+function arrayOfProducts(array) {
+  console.log('array =', array);
+  const products = new Array(array.length).fill(1);
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+  let leftRunningProduct = 1;
+  for (let i = 0; i < array.length; i++) {
+    console.log('index =', i);
+    console.log(products, 'products before');
+    console.log(leftRunningProduct, 'leftRunningProduct before');
+    products[i] = leftRunningProduct;
+    leftRunningProduct *= array[i];
+    console.log(leftRunningProduct, 'leftRunningProduct after');
+    console.log(products, 'products after');
+  }
 
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
+  let rightRunningProduct = 1;
+  for (let i = array.length - 1; i > -1; i--) {
+    console.log('index =', i);
+    console.log(products, 'products2 before');
+    console.log(rightRunningProduct, 'rightRunningProduct before');
+    products[i] *= rightRunningProduct;
+    rightRunningProduct *= array[i];
+    console.log(rightRunningProduct, 'rightRunningProduct after');
+    console.log(products, 'products2 after');
+  }
+  return products;
 }
 
-/*
- * Complete the 'plusMinus' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
-function plusMinus(arr) {
-    let numberStore = {};
-    let length = array.length;
-    
-    for (let i; i <= arr.length; i++) {
-        if (arr[i] < 0) {
-           console.log(arr[i]);
-           numberStore.negative += 1;
-        } else if (arr[i] > 0) {
-            numberStore.positive += 1;
-        }
-        else numberStore.zero += 1;
-    }
-    
-    console.log(numberStore.negative);
-    console.log(numberStore.positive);
-    console.log(numberStore.zero);
-}
-
-function main() {
-    const n = parseInt(readLine().trim(), 10);
-
-    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
-
-    plusMinus(arr);
-}
+arrayOfProducts([5, 4, 3, 2, 1]);
